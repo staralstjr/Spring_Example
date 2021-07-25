1413d46ee357d649eff0b838ebeabd7d2a6cc500
staralstjr <staralstjr@gmail.com>
2021-07-25 17:06:22 +0900

    [권민석] 마론달 예제 1 - SELECT 다루기 에서 1, 2, 3번 문제 마무리

--- src/main/java/com/staralstjr/spring/lesson03/ReviewController.java
+++ src/main/java/com/staralstjr/spring/lesson03/ReviewController.java
@@ -2,6 +2,7 @@ package com.staralstjr.spring.lesson03;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.staralstjr.spring.lesson03.bo.ReviewBO;
@@ -13,9 +14,19 @@ public class ReviewController {
 	private ReviewBO reviewBO;
 	
 	@RequestMapping("/lesson03/ex01")
-	public Review ex01() {
+	// /lesson03/ex01?id=3
+	public Review ex01(
+			@RequestParam("id") int id // value, required=true 포함된 심플
+			// @RequestParam(value="id")int id // 필수 없으면 400에러
+			// @RequestParam(value="id", required=true)int id // 필수 없으면 400에러
+			// @RequestParam(value="id", required=false)int id // 필수 아님
+			// @RequestParam(value="id", defaultValue="1")int id // 필수 없으면 id가 자동으로 1을 출력!
+	
+			
+			
+			) {
 		
-		return reviewBO.getReview(3);
+		return reviewBO.getReview(id);
 		
 	}
 }
--- src/main/java/com/staralstjr/spring/lesson03/model/Review.java
+++ src/main/java/com/staralstjr/spring/lesson03/model/Review.java
@@ -13,4 +13,55 @@ public class Review {
 	private Date updatedAt;
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public double getPoint() {
+		return point;
+	}
+	public void setPoint(double point) {
+		this.point = point;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
 }
